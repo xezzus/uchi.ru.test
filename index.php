85716045d9888246cc78b15e439b02ceab3cd1cc
xezzus <xezzus@gmail.com>
2017-12-25 00:22:23 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<!doctype html>
+
+<html lang="ru">
+<head>
+  <meta charset="utf-8">
+
+  <title>Тестовое задание для uchi.ru</title>
+  <meta name="description" content="Тестовое задание для uchi.ru">
+  <meta name="author" content="Andrey Buturlakin">
+
+  <link rel="stylesheet" href="normalize.css">
+  <link rel="stylesheet" href="index.css?<?php echo microtime(1); ?>">
+
+</head>
+
+<body>
+
+  <div id="doc"></div>
+
+  <script src="svg.min.js"></script>
+  <script src="svg.foreignobject.js"></script>
+  <script src="jquery-3.2.1.min.js"></script>
+  <script src="index.js?<?php echo microtime(1); ?>"></script>
+</body>
+</html>
